3a33bd57c0983de8fe51704c08d8da88d7b45450
user at sakaki <user@sakaki>
2016-03-02 02:06:40 +0300

    Add CMakeLists.txt

---
+++ CMakeFiles/final.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/final.dir/final.c.o"
+  "final.pdb"
+  "final"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
